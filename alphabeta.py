906222e42952ec8edcebf2b3ea983609c0eff31a
Samar Joshi <167465181+Samar2355@users.noreply.github.com>
2026-03-25 12:08:58 +0200

    Alpha beta algorithm
    Although like 60-70% of it is like copy pasted from the min max algorithm

--- alphabeta.py
+++ alphabeta.py
@@ -1,55 +1,61 @@
 from Node import Node
+visited_edges = [] #for seeing the edges that are visited
 
-#To-Do: Change this minimax to alphabeta algorithm
-def build_tree(node, depth=0, max_depth=6):
-    if node.number <= 10 or depth >= max_depth:
+def build_tree(node, depth=0, max_depth=6): # this is defining the function for the tree where node is node, depth is the current depth and maax_depth is 6
+    if node.number <= 10 or depth >= max_depth: # this is fo the depth that if <=10 which is the final condition of game is reached or max depth which is 6 is reached then stop
         return
 
-    Node.generate_children(node)
+    Node.generate_children(node) #makes node
 
     for child in node.children:
-        build_tree(child, depth + 1, max_depth)
+        build_tree(child, depth + 1, max_depth) #for each node level increase the depth level by 1 
 
-def minimax(node):
-    # terminal node
-    if node.number <= 10 or not node.children:
-        # give bank to last player
+def alpha_beta(node,alpha,beta): #definining alpha beta algorithm
+    global visited_edges
+    if node.number <= 10 or not node.children: 
         if node.is_pc_turn:
             node.pc_score += node.bank
         else:
-            node.player_score += node.bank
+            node.player_score += node.bank #these statement are mainly for the bank 
 
-        node.value = node.pc_score - node.player_score
+        node.value = node.pc_score - node.player_score #this checks for the value of a node, if its higher than 0, its better for pc, if lower than 0 then better for player
         return node.value
-
+    
     if node.is_pc_turn:
-        best = -float("inf")
+        best = -float("inf") #if is pc turn, start with the worse possible value so that it any value found after this will be better
 
         for child in node.children:
-            val = minimax(child)
-            best = max(best, val)
-
-        node.value = best
+            visited_edges.append((node.name,child.name, "visit")) #append each node as visited
+            val = alpha_beta(child,alpha,beta)# checks for the value of the child
+            best = max(best,val)
+            alpha = max(alpha, best) #change the value to best value found
+            if beta <= alpha:
+                visited_edges.append((node.name, child.name, "cut"))
+                break #cutoff, if beta <= then it wont be considered
+        node.value = best #node value now to be the best value now
         return best
 
     else:
-        best = float("inf")
-
+        best = float("inf") # same as before but for player turn where the worse value is +inf
+        
         for child in node.children:
-            val = minimax(child)
-            best = min(best, val)
-
+            visited_edges.append((node.name,child.name, "visit"))
+            val = alpha_beta(child,alpha,beta)
+            best = min(best,val)
+            beta = min(beta, best) #similar thing like before, but take the minimum value instead of max for beta
+            if beta <= alpha:
+                visited_edges.append((node.name, child.name, "cut"))
+                break #same, if beta is less than alpha then break
         node.value = best
         return best
 
-def get_best_move_from_tree(root):
-    best_value = -float("inf")
-    best_move = None
+def best_move(root):
+    best_value = -float("inf") #so -inf because  so it assumes the worst case, any value found will be better
+    best_move = None #none is basically a placeholder
 
-    for child in root.children:
-        if child.value > best_value:
-            best_value = child.value
-            best_move = child.move
+    for child in root.children: 
+        if child.value > best_value: #this checks if the value of the node is better than the value of the previous node, which at the start will be true as best_value will be -inf 
+            best_value = child.value #now if the value is better, it will then be considered as the child node
+            best_move = child.move #and the best move will be considered
 
     return best_move
-
